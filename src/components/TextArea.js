c7867c036d22b544d4a7f3dfbbb8eb670936adcd
Taylor Laekeman <taylor.laekeman@gmail.com>
2019-11-13 21:11:59 -0500

    Unformatted address (#15)
    * change shipping form to use a single text area for the address
    
    * remove error box shadow from text area
    
    * add error hint to text area
    
    * simplify checkout form flow
    
    * add form field component
    
    * fix lint errors on checkout page

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -2,38 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import boxShadow from '../styles/boxShadow';
 import colours from '../styles/colours';
+import fonts from '../styles/fonts';
 import FormField, { hasVisibleError, replaceColourIfError } from './FormField';
 
-const StyledInput = styled.input`
+const StyledTextArea = styled.textarea`
   box-sizing: border-box;
   border-radius: 0;
   width: 100%;
   padding: 12px;
-  border: none;
-  border-bottom: solid ${props => replaceColourIfError(colours.grey['300'], props)} 1px;
-  font-size: 1.2rem;
-  font-weight: 300;
+  border: solid ${props => replaceColourIfError(colours.grey['300'], props)} 1px;
+  border-radius: 5px;
+  font-family: ${fonts.body}, ${fonts.fallback};
+  font-size: 1.1rem;
   color: ${props => replaceColourIfError(colours.green['600'], props)};
-  box-shadow: ${boxShadow.medium};
   -webkit-appearance: none;
+  resize: none;
+  box-shadow: none;
 
   &:focus {
     outline: none;
   }
 `;
 
-const Input = ({
+const TextArea = ({
   area,
-  areErrorsVisible,
   className,
+  areErrorsVisible,
   isRequired,
   label,
-  maxLength,
-  minLength,
   onChange,
-  type,
+  rows,
   validity,
   value,
 }) => (
@@ -42,40 +41,28 @@ const Input = ({
     className={className}
     hasVisibleError={hasVisibleError(value, validity, areErrorsVisible)}
     label={label}
-    minLength={minLength}
-    type={type}
     validity={validity}
   >
-    <StyledInput
+    <StyledTextArea
       hasVisibleError={hasVisibleError(value, validity, areErrorsVisible)}
       id={label}
       name={label}
-      maxLength={maxLength}
-      minLength={minLength}
       onChange={event => onChange({ value: event.target.value, validity: event.target.validity })}
       required={isRequired}
-      type={type}
+      rows={rows}
       value={value}
     />
   </FormField>
 );
 
-Input.propTypes = {
+TextArea.propTypes = {
   area: PropTypes.string,
   areErrorsVisible: PropTypes.bool,
   className: PropTypes.string,
   isRequired: PropTypes.bool,
   label: PropTypes.string.isRequired,
-  maxLength: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-  ]),
-  minLength: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-  ]),
   onChange: PropTypes.func.isRequired,
-  type: PropTypes.string,
+  rows: PropTypes.number,
   validity: PropTypes.shape({
     badInput: PropTypes.bool.isRequired,
     customError: PropTypes.bool.isRequired,
@@ -94,14 +81,12 @@ Input.propTypes = {
   ]).isRequired,
 };
 
-Input.defaultProps = {
+TextArea.defaultProps = {
   area: '',
   areErrorsVisible: true,
   className: '',
   isRequired: false,
-  maxLength: '',
-  minLength: '',
-  type: 'text',
+  rows: 3,
 };
 
-export default Input;
+export default TextArea;
