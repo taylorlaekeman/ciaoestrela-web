1acadde8e6f99dd8102661698882a2177207e365
Taylor Laekeman <taylor.laekeman@gmail.com>
2020-02-15 17:51:38 -0500

    setup redux thunk

--- src/store/config.js
+++ src/store/config.js
@@ -1,10 +1,12 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+
 import { reducers as cartReducers } from './cart';
 
 const reducers = combineReducers({
   cart: cartReducers,
 });
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 export default store;
