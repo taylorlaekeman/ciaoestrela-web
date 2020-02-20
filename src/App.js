4e744ea74f166bd3c0994b66fc341c120d6741dd
Taylor Laekeman <taylor.laekeman@gmail.com>
2020-02-19 20:40:30 -0500

    move style imports below component imports in app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 import { ThemeProvider } from 'styled-components';
 
-import theme from 'styles';
-import GlobalStyle from 'styles/globalStyle';
 import AboutPage from 'components/AboutPage';
 import CartPage from 'components/CartPage';
 import CheckoutPage from 'components/CheckoutPage';
@@ -12,6 +10,8 @@ import GalleryPage from 'components/GalleryPage';
 import Header from 'components/Header';
 import HomePage from 'components/HomePage';
 import OrderPage from 'components/OrderPage';
+import GlobalStyle from 'styles/globalStyle';
+import theme from 'styles';
 
 function App() {
   return (
