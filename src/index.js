a0595fba2e12acb89c8b50649e3e9de04bcb865f
Taylor Laekeman <taylor.laekeman@gmail.com>
2020-02-19 20:41:29 -0500

    update imports in index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import store from './store/config';
+import App from 'App';
+import * as serviceWorker from 'serviceWorker';
+import store from 'store/config';
 
 ReactDOM.render(
   <Provider store={store}>
