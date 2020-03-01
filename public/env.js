31fe73fba87f8dcde932ae51033393e0e8810848
Taylor Laekeman <taylor.laekeman@gmail.com>
2020-02-29 21:24:29 -0500

    switch to stripe live key (#28)

--- public/env.js
+++ public/env.js
@@ -1,3 +1,5 @@
 window.env = {
-  API_URL: 'https://api.ciaoestrela.co'
+  API_URL: 'https://api.ciaoestrela.co',
+  STRIPE_PUBLIC_KEY: 'pk_live_oPtewnYrSvVssrtWd3CZj9ZU00L4iDmAvl',
+  //STRIPE_PUBLIC_KEY: 'pk_test_FPDcKMYO8kbyugztNmLNqSvL00mU17Lbav',
 };
